cdca5453d1e78f88508d60253560f419608c5f64
Johnny Chen <96746703+JohnnyChen129@users.noreply.github.com>
2023-04-04 10:05:23 -0400

    changed cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 //middleware
-SVGClipPathElement.arguments(cors());
+app.use(cors());
 app.use(express.json());
 
 app.listen(5000, () => {
